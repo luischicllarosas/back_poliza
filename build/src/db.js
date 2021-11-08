c3b02efca838f4f431e285c8722e1351db97b759
LuisPC <luis@lamediadl.com>
2021-11-08 08:50:08 -0500

    fix dir build && comand line for run prod ~ build

--- build/src/db.js
+++ build/src/db.js
@@ -1,1 +1,7 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const env_1 = require("./env");
+(0, mongoose_1.connect)(env_1.mongodb.URI);
+mongoose_1.connection.once('open', _ => console.info('DB is connected', env_1.mongodb.URI));
+mongoose_1.connection.once('error', err => console.info('Error in : ', err.message));
